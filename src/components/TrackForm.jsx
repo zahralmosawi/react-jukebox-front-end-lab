3a9b048d79d5f61558124804d7fe8a0bbb30105a
zahralmosawi <zalmosawii65@gmail.com>
2025-08-15 20:10:54 +0300

    added handle change and submit functions

--- src/components/TrackForm.jsx
+++ src/components/TrackForm.jsx
@@ -3,21 +3,30 @@ import { useState } from "react"
 import {create} from '../../lib/api'
 
 const TrackForm = ({setFormIsShown}) => {
+
     const [formData, setFormData] = useState({
         title: '',
         artist: ''
     })
 
+    function handleChange(event){
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    async function handleSubmit(event){
+        event.preventDefault()
+    }
+
     return(
         <>
         <h2>Add New Track</h2>
         
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="title">Title:</label>
-            <input type="text" id="title" name="tilte" value={formData.title}/>
+            <input type="text" id="title" name="tilte" value={formData.title} onChange={handleChange}/>
             
             <label htmlFor="artist">Artist:</label>
-            <input type="text" id="artist" name="artist" value={formData.artist}/>
+            <input type="text" id="artist" name="artist" value={formData.artist} onChange={handleChange}/>
 
             <button type="submit">Submit</button>
         </form>
